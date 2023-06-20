d7e71487a6630c0d5aa48615f6587009186bfd91
Carlos-Simim <42616810+Carlos-Simim@users.noreply.github.com>
2023-06-20 07:39:09 -0300

    Update requests.js
    Fix base_url

--- src/utils/requests.js
+++ src/utils/requests.js
@@ -1,4 +1,4 @@
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://lp3-backend.herokuapp.com/api/v1';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'https://lp3-backend.herokuapp.com/api/v1';
 
 export function parseData(data){
     var ano = data.toString().split(',')[0];
