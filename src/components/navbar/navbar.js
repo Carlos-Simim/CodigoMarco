b18660f219c5704f746c3775f1f892ca8dbeb9b5
Lucas wilman da silva crispim <Lucas Wilman da Silva Crispim>
2023-07-04 13:45:22 -0300

    remove email from localstorage

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -1,38 +1,42 @@
-import React from 'react';
+import React from "react";
 import { useNavigate, Link } from "react-router-dom";
-import { doLogout } from '../../services/authservice';
+import { doLogout } from "../../services/authservice";
 
 const Navbar = (props) => {
-
   const navigate = useNavigate();
 
   const cleanAndRedirect = () => {
-    localStorage.removeItem('token');
-    navigate('/');
-  }
+    localStorage.removeItem("token");
+    localStorage.removeItem("email");
+    navigate("/");
+  };
 
   const onLogoutClick = (event) => {
-    doLogout(localStorage.getItem('token'))
-      .then(response => cleanAndRedirect())
-      .catch(error => {
+    doLogout(localStorage.getItem("token"))
+      .then((response) => cleanAndRedirect())
+      .catch((error) => {
         console.error(error);
         cleanAndRedirect();
-      })
-  }
+      });
+  };
 
   const getTo = (toName) => {
     return window.location.pathname === toName ? true : false;
   };
 
-
   return (
-    <div className='navbar fixed-top rounded border-dark bg-black'>
-      <div className='container'>
-
-        <h2 className='navbar-brand text-white' onClick={() => navigate("/lista-carteiras")} style={{ cursor: 'pointer' }}> Gerenciador de carteiras </h2>
+    <div className="navbar fixed-top rounded border-dark bg-black">
+      <div className="container">
+        <h2
+          className="navbar-brand text-white"
+          onClick={() => navigate("/lista-carteiras")}
+          style={{ cursor: "pointer" }}
+        >
+          {" "}
+          Gerenciador de carteiras{" "}
+        </h2>
 
         <div className="button-group">
-
           {props?.listarAtivos ? (
             <Link
               style={{ textDecoration: "none", marginRight: 10 }}
@@ -41,23 +45,25 @@ const Navbar = (props) => {
             >
               Listar Ativos
             </Link>
-          ) : ''}
-
-          {
-            props?.deslogar ?
-              (
-                <button
-                  className="btn btn-outline-success my-2 my-sm-0"
-                  onClick={onLogoutClick}
-                  type="submit"
-                >
-                  Sair
-                </button>
-              ) : ''};
+          ) : (
+            ""
+          )}
+          {props?.deslogar ? (
+            <button
+              className="btn btn-outline-success my-2 my-sm-0"
+              onClick={onLogoutClick}
+              type="submit"
+            >
+              Sair
+            </button>
+          ) : (
+            ""
+          )}
+          ;
         </div>
       </div>
     </div>
   );
-}
+};
 
 export default Navbar;
--- src/services/authservice.js
+++ src/services/authservice.js
@@ -1,18 +1,18 @@
-import axios from './baseservice';
+import axios from "./baseservice";
 import { BASE_URL } from "../utils/requests";
 
-
 export async function doLogin(email, senha) {
-    const loginUrl = `${BASE_URL}/usuario/auth`;
-    const response = await axios.post(loginUrl, { email: email, senha: senha });
-    return response.data;
+  const loginUrl = `${BASE_URL}/usuario/auth`;
+  const response = await axios.post(loginUrl, { email: email, senha: senha });
+  return response.data;
 }
 
 export async function doLogout(token) {
-    localStorage.removeItem('token');
-    // const logoutUrl = `${BASE_URL}/logout`;
-    // const headers = { 'authorization': token };
-    // const response = await axios.post(logoutUrl, {}, { headers });
-    // return response.data;
-    return {msg:'ok',status:200}
+  localStorage.removeItem("token");
+  localStorage.removeItem("email");
+  // const logoutUrl = `${BASE_URL}/logout`;
+  // const headers = { 'authorization': token };
+  // const response = await axios.post(logoutUrl, {}, { headers });
+  // return response.data;
+  return { msg: "ok", status: 200 };
 }
